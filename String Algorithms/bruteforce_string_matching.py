8f2dfd6833b78ec95e7b774871de0e3b52bf5bba
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2024-03-25 12:13:40 +0000

    Add genome.py and implement brute force string matching algorithm

--- String Algorithms/bruteforce_string_matching.py
+++ String Algorithms/bruteforce_string_matching.py
@@ -1,8 +1,18 @@
-from Genome.genome import Genome
-
+from genome import Genome
 genome = Genome("panamabananas")
 pattern = "nana"
 
 def brtforce(ptrn:str, genome: Genome):
-    for sequence in genome.sequences:
+    sequences = genome.sequences
+    for pos, sequence in enumerate(sequences):
         print(sequence)
+        if sequence == ptrn[0]:
+            subseq = sequences[pos: len(ptrn)+2]
+            print('subsque is ', subseq)
+            if subseq == ptrn:
+                print(f"Pattern found at position {pos}")
+            else:
+                print(f"Pattern not found at position {pos}") 
+
+brtforce(pattern, genome)
+
